51bb039729dde3f5f2a7d32ca924db0af0e6a10d
”abdelrahman <eabdo6990@gmail.com>
2026-07-28 16:53:00 +0300

    make the database connection

---
+++ src/app.controller.js
@@ -0,0 +1,15 @@
+import dbconeection from "./DB/db.connection.js";
+import cors from "cors";
+
+const bootstrap = (app, express) => {
+  app.use(express.json());
+  app.use(cors());
+
+  app.all("/*splat", (req, res) => {
+    res.status(404).json({ message: "Not Found" });
+  });
+  //   app.use(globalErrorhandling);
+  dbconeection();
+};
+
+export default bootstrap;
