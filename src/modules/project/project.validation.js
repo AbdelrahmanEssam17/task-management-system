d48ff8be0cbe723c3133d186e4ea49ca0ce08cce
”abdelrahman <eabdo6990@gmail.com>
2026-07-28 18:58:37 +0300

    middleware to ckeck roleType + project validation

---
+++ src/modules/project/project.validation.js
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+export const createProject = Joi.object({
+  name: Joi.string().min(3).max(100).required(),
+
+  description: Joi.string().max(500).optional(),
+});
+
+export const updateProject = Joi.object({
+  name: Joi.string().min(3).max(100).optional(),
+
+  description: Joi.string().max(500).optional(),
+});
