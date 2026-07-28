67d98f15b45cfd408511f324a945a383837725ff
”abdelrahman <eabdo6990@gmail.com>
2026-07-28 18:03:19 +0300

    middlewate to check emailExist

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -4,7 +4,13 @@ import validation from "../../middleware/validation.js";
 import * as authValidation from "./auth.validation.js";
 import { auth as verifyToken } from "../../middleware/auth.middleware.js";
 const router = express.Router();
-router.post("/register", validation(authValidation.register), auth.register);
+import { emailExist } from "../../middleware/isExist.js";
+router.post(
+  "/register",
+  emailExist,
+  validation(authValidation.register),
+  auth.register,
+);
 router.post("/login", validation(authValidation.login), auth.login);
 
 export default router;
